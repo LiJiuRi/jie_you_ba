7eb9e53fc386de840e923484e617801cade206b4
yz-lixu <1402654659@qq.com>
2019-04-01 21:41:52 +0800

    订单管理

--- src/main/java/com/lixu/jie_you_ba/entity/Order.java
+++ src/main/java/com/lixu/jie_you_ba/entity/Order.java
@@ -39,7 +39,7 @@ public class Order {
 
     private String createPerson;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern="yyyy.MM.dd HH:mm",timezone = "GMT+8")
     private Date createTime;
 
     private String updatePerson;
